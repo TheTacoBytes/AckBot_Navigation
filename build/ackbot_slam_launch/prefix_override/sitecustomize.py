fc7f9caa61d9d9aaf19a8450c5bbd046f19054e4
Humberto Ruiz <thetacobytes@gmail.com>
2024-09-30 00:36:24 -0400

    Remove build, install, and log directories from repository

--- build/ackbot_slam_launch/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/beto/ackbot_ws/install/ackbot_slam_launch'
--- build/ps4_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/beto/ackbot_ws/install/ps4_controller'
